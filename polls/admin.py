ba68801476d8781bc5229159c0747e05874e7e0d
saumyasinghz <saumyasingh3103@gmail.com>
2025-10-22 14:41:20 -0400

    Initial commit for Django tutorial project

---
+++ polls/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+# Optionally expose Choice directly too
+# admin.site.register(Choice)
